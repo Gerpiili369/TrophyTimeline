c72a929c935d1e47f3f8ddf4d7492f55f8c5cc5a
Gerpiili369 <elektromiikka@hotmail.com>
2018-07-26 17:38:53 +0200

    moved public files

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const exp = require('express'), app = exp();
-app.use('/', exp.static(__dirname));
+const path = require('path'), exp = require('express'), app = exp();
+app.use('/', exp.static(path.join(__dirname, 'public')));
 require('http').Server(app).listen(80, '127.0.0.1', () => console.log(`listening...`));
