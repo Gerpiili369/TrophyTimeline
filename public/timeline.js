fdd0029077e3f442cea0daf886c6adbfe6ec6045
Gerpiili369 <elektromiikka@hotmail.com>
2018-07-31 14:33:36 +0200

    1 canvas instead of 3

--- public/timeline.js
+++ public/timeline.js
@@ -1,15 +1,7 @@
 (() => document.addEventListener('DOMContentLoaded', () => {
     const
-        canvas = {
-            a: document.getElementById('canvas'),
-            b: document.getElementById('block'),
-            h: document.getElementById('hud')
-        },
-        context = {
-            a: canvas.a.getContext('2d'),
-            b: canvas.b.getContext('2d'),
-            h: canvas.h.getContext('2d')
-        },
+        canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
         elements = {
             title: document.getElementById('title'),
             text: document.getElementById('text'),
@@ -25,6 +17,8 @@
 
     elements.zoom.onchange = () => drawTL(...upData);
     // for (zoom of elements.zoom) zoom.addEventListener('click', () => drawTL(...upData));
+    canvas.onmousemove = e => drawTL(...upData, e);
+    canvas.addEventListener('click', e => drawTL(...upData, e, true), false);
 
     elements.button.addEventListener('click', () => {
         if (ready) {
@@ -61,7 +55,7 @@ function status(elements, title, text = '', cursor = 'auto') {
 
 function search(canvas, context, elements) {
     return new Promise((resolve, reject) => {
-        const object = {}, gameList = [], dateList = [];
+        const object = {}, games = {}, dateList = [];
         let trophyList;
         fetch(elements.username.value === '' ? 'demo.json' : `http://localhost:3004/api/trophies/${elements.username.value}?groupByGame=true&groupByDate=year`)
             .then(result => result.json())
@@ -72,12 +66,23 @@ function search(canvas, context, elements) {
                         object[new Date(year.title).getFullYear()] = [];
                         for (trophyData of year.list) {
                             trophyList = [];
-                            if (gameList.indexOf(trophyData.title) == -1) gameList.push(trophyData.title);
+                            games[trophyData.title] = null;
                             for (game of trophyData.list) trophyList.push(game.trophy);
                             object[new Date(year.title).getFullYear()].push({game: trophyData.title, list: trophyList});
                         }
                     }
-                    upData = [object, gameList, canvas, context, elements]
+                    for (const game in games) {
+                        const min = 100, colors = [0, 0, 0];
+
+                        while((colors[0] + colors[1] + colors[2]) < min) {
+                            for (color in colors) colors[color] = Math.floor(Math.random() * 256);
+                        }
+
+                        for (color in colors) colors[color] = colors[color].toString(16);
+                        const hex = colors.join('');
+                        games[game] = hex.length < 6 ? hex.length < 5 ? '00' : '0' + hex : hex;
+                    }
+                    upData = [object, games, canvas, context, elements];
                     drawTL(...upData, );
                     resolve(data.username);
                 }
@@ -86,9 +91,9 @@ function search(canvas, context, elements) {
     });
 }
 
-function drawTL(object, gList, canvas, c, elements) {
-    const start = msToH(new Date(new Date().getFullYear() + 1, 0)), gc = {}, boxes = [];
-    let x, y = Object.keys(object).length * 50, zoom;
+function drawTL(object, games, canvas, c, elements, e, click = false) {
+    const start = msToH(new Date(new Date().getFullYear() + 1, 0));
+    let x, y = Object.keys(object).length * 50, zoom, ctos, text = '';
 
     switch(elements.zoom.value) {
     // for (i of elements.zoom) if (i.checked) switch (i.value) {
@@ -101,117 +106,76 @@ function drawTL(object, gList, canvas, c, elements) {
         default: console.log(elements.zoom.value);
     }
 
-    canvas.a.height = y;
-    canvas.b.height = y;
-    canvas.h.height = y;
+    canvas.height = y;
+    canvas.width = 8760 * zoom;
 
-    canvas.a.width = 8760 * zoom;
-    canvas.b.width = 8760 * zoom;
-    canvas.h.width = 8760 * zoom;
+    c.clearRect(0, 0, canvas.width, canvas.height);
 
-    c.a.clearRect(0, 0, canvas.b.width, canvas.b.height);
-    c.b.clearRect(0, 0, canvas.b.width, canvas.b.height);
-    c.h.clearRect(0, 0, canvas.b.width, canvas.b.height);
+    c.textBaseline = 'alphabetic';
+    c.font = '12pt Serif';
 
-    for (i = 0, min = 100, colors = []; i < gList.length; i++) {
-        colors = [0, 0, 0];
-        while((colors[0] + colors[1] + colors[2]) < min) {
-            for (color in colors) colors[color] = Math.floor(Math.random() * 256);
-        }
-
-        for (color in colors) colors[color] = colors[color].toString(16);
-        hex = colors.join('');
-        gc[gList[i]] = hex.length < 6 ? hex.length < 5 ? '00' : '0' + hex : hex;
-    }
     for (year in object) {
         y -= 50;
-        for (date of object[year]) {
-            date.start = msToH(date.list[0].earned);
-            date.end = msToH(date.list[date.list.length - 1].earned);
-            date.extra = (start - msToH(new Date(new Date(date.list[0].earned).getFullYear() + 1, 0)));
+        object[year].reverse();
+        for (gs of object[year]) {
+            gs.start = msToH(gs.list[0].earned);
+            gs.end = msToH(gs.list[gs.list.length - 1].earned);
+            gs.extra = (start - msToH(new Date(new Date(gs.list[0].earned).getFullYear() + 1, 0)));
 
-            x = (start - date.start - date.extra) * zoom;
-            width = (date.start - date.end) * zoom;
+            x = (start - gs.start - gs.extra) * zoom;
+            width = (gs.start - gs.end) * zoom;
 
             if (width < 1 || width > 100000) width = 1 * zoom;
 
-            c.a.save();
-            c.a.strokeStyle = '#'+ gc[date.game];
-            c.a.strokeRect(x, y, width, 45);
-            c.a.restore();
+            if (e) {
+                const rect = canvas.getBoundingClientRect(),
+                    mx = e.clientX - rect.left, my = e.clientY - rect.top;
 
-            c.b.save();
-            c.b.fillStyle = '#'+ gc[date.game];
-            c.b.fillRect(x, y, width, 45);
-            c.b.restore();
+                c.beginPath();
+                c.rect(x, y, width, 45);
+                if (c.isPointInPath(mx, my)) {
+                    box(c, x, y, games[gs.game], width, true);
 
-            boxes.push({x: x, y: y, w: width, h: 45, t: date.game, d: date.list});
+                    ctos = 35;
+                    c.fillText(gs.game, mx + 10, my + ctos);
 
-            for (trophy of date.list) {
-                x = (start - msToH(trophy.earned) - date.extra) * zoom;
-                c.a.save();
-                c.a.strokeStyle = '#'+ gc[date.game];
-                c.a.strokeRect(x, y, 0, 40);
-                c.a.restore();
-                boxes.push({x: x, y: y, w: 1, h: 40, t: date.game, n: trophy.title, d: date.list});
-            }
+                    for (trophy of gs.list) {
+                        x = (start - msToH(trophy.earned) - gs.extra) * zoom;
+
+                        box(c, x, y, games[gs.game]);
+
+                        c.beginPath();
+                        c.rect(x, y, 1, 40);
+                        if (c.isPointInPath(mx, my)) {
+                            ctos += 20;
+                            c.fillText(trophy.title, mx + 10, my + ctos);
+                        }
+
+                        text += trophy.title + '\n';
+                    }
+                    if (click) status(elements, gs.game, text);
+                } else box(c, x, y, games[gs.game], width);
+            } else box(c, x, y, games[gs.game], width);
         }
     }
 
-    for (i = 0; i < canvas.a.width; i += 24 * zoom) {
-        c.a.save();
-        c.a.fillStyle = 'green';
-        c.a.fillRect(i, 0, 1, 25);
-        c.a.restore();
+    document.body.style.cursor = ctos ? 'help' : 'auto';
+
+    for (i = 0; i < canvas.width; i += 24 * zoom) box(c, i);
+}
+
+function box(c, x, y, color, width, empty) {
+    c.save();
+    if (color) {
+        c.strokeStyle = '#' + color;
+        c.fillStyle = '#' + color;
+        if (width) {
+            if (empty) c.strokeRect(x, y, width, 45);
+            else c.fillRect(x, y, width, 45);
+        } else c.strokeRect(x, y, 0, 40);
+    } else {
+        c.fillStyle = 'green';
+        c.fillRect(x, 0, 1, 25);
     }
-
-    canvas.h.onmousemove = e => {
-        c.b.clearRect(0, 0, canvas.b.width, canvas.b.height);
-        const
-            rect = canvas.h.getBoundingClientRect(),
-            x = e.clientX - rect.left, y = e.clientY - rect.top;
-        let i = 0, r, succ = false;
-
-        while(r = boxes[i++]) {
-            c.h.beginPath();
-            c.h.rect(r.x, r.y, r.w, r.h);
-
-            if (c.h.isPointInPath(x, y)) {
-                succ = true;
-                c.h.clearRect(0, 0, canvas.a.width, canvas.a.height)
-                c.h.save();
-        		c.h.textBaseline = 'alphabetic';
-        		c.h.font = '12pt Serif';
-        		c.h.fillText(r.t, x + 10, y + 35);
-                r.n ? c.h.fillText(r.n, x + 10, y + 55) : '';
-                c.h.restore();
-                document.body.style.cursor = 'help';
-            } else {
-                c.b.save();
-                c.b.fillStyle = '#'+ gc[r.t];
-                c.b.fillRect(r.x, r.y, r.w, r.h);
-                c.b.restore();
-            }
-
-        }
-        if (!succ) {
-            document.body.style.cursor = 'auto';
-            c.h.clearRect(0, 0, canvas.a.width, canvas.a.height);
-        }
-    }
-
-    canvas.h.addEventListener('click', e => {
-        const rect = canvas.h.getBoundingClientRect();
-        let i = 0, r, text = '';
-
-        while(r = boxes[i++]) {
-            c.h.beginPath();
-            c.h.rect(r.x, r.y, r.w, r.h);
-
-            if (c.h.isPointInPath(e.clientX - rect.left, y = e.clientY - rect.top)) {
-                for (trophy of r.d) text += trophy.title + '\n';
-                status(elements, r.t, text);
-            }
-        }
-    }, false);
+    c.restore();
 }
